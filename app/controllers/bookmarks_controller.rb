a7d783f1b1b7bed1b05ec04104d7d1415e6509e6
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2023-05-26 15:29:03 +0100

    Add dotenv-rails

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,7 +15,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@list)
+    redirect_to list_path(@bookmark.list)
   end
 
   private
