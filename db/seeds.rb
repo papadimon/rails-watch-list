bb57eb29eb4cd69eeee8602433194223c6a62a8f
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2023-05-26 16:03:25 +0100

    all done

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ movies["results"].each do |movie|
   Movie.create!(
     title: movie["original_title"],
     overview: movie["overview"],
-    poster_url: movie["url"],
+    poster_url: movie["poster_path"],
     rating: movie["vote_average"]
   )
 end
